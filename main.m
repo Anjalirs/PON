ebbe788105213ca242440733efaf808e2ead283f
Anjalirs <51824316+Anjalirs@users.noreply.github.com>
2022-05-14 13:10:18 +0530

    Create main.m

---
+++ main.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+close all;
+                                                                            %% Initializing some parameters
+
+No_of_iterations =20;                                                         % Repeating the whole simulation for "No_of_iterations" times and then Averaging them.
+
+                                                                             %% defining "optical_network" 
+                                                                             % Any network can be written in matrix form (as shown below). You can save
+                                                                             % this matix and then load this file while doing simulations as its been
+                                                  
+   %%%%%%%%%DEFINING NETWORK TOPOLOGIES
+                                                                             %% simple 4 node- directed
+  optical_network =[inf 1 inf inf;
+                     inf inf inf 1;
+                     1 1 inf inf;
+                     inf inf 1 inf]; 
